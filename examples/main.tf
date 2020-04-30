25016078869ce5a0136738c200479496ab1960fb
Tim Arenz <tim@arenz.cc>
2020-04-30 12:40:56 +0200

    Add simple example

---
+++ examples/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  features {}
+}
+
+module "hcs" {
+  source              = "..."
+  resource_group_name = "my-rg-for-consul"
+  application_name    = "hcs"
+  consul_cluster_name = "my-consul-cluster"
+  external_endpoint   = true
+}
+
+output "hcs_config_command" {
+  value = module.hcs.get_hcs_config_command
+}
